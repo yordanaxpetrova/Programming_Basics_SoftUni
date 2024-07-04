c30c4ee052199516d3000c63842e8a064419be1f
Yordana Petrova <yordanaxpetrova3@gmail.com>
2024-07-04 12:17:41 +0300

    Create 07.ProjectsCreation.cs

---
+++ 01. First steps in programming/07.ProjectsCreation.cs
@@ -0,0 +1,4 @@
+string name = Console.ReadLine();
+int projects = int.Parse(Console.ReadLine());
+int hours = projects * 3;
+Console.WriteLine($"The architect {name} will need {hours} hours to complete {projects} project/s.");
