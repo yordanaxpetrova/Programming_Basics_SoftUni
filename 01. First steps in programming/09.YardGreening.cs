ccdb730b4c524df38cb482e26a4b21be08d72422
Yordana Petrova <yordanaxpetrova3@gmail.com>
2024-07-04 12:18:27 +0300

    Create 09.YardGreening.cs

---
+++ 01. First steps in programming/09.YardGreening.cs
@@ -0,0 +1,7 @@
+double yardArea = double.Parse(Console.ReadLine());
+double price = yardArea * 7.61;
+double discount = price * 0.18;
+double finalPrice = price - discount;
+
+Console.WriteLine($"The final price is: {finalPrice} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
